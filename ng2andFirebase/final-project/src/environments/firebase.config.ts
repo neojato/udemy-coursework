82a358c895c2db2e78bb4aa259217f75135462f1
Kyle Paul <kpaul86+github@gmail.com>
2017-07-31 20:17:39 -0500

    Update firebase.config.ts

--- ng2andFirebase/final-project/src/environments/firebase.config.ts
+++ ng2andFirebase/final-project/src/environments/firebase.config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDwbU-5_fzO2R7yqNvt7cmJPDbn2iOMQPE",
-  authDomain: "fir-course-712e5.firebaseapp.com",
-  databaseURL: "https://fir-course-712e5.firebaseio.com",
-  projectId: "fir-course-712e5",
-  storageBucket: "fir-course-712e5.appspot.com",
-  messagingSenderId: "964352873663"
+  apiKey: "<API_KEY>",
+  authDomain: "<PROJECT_ID>.firebaseapp.com",
+  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+  projectId: "<PROJECT_ID>",
+  storageBucket: "<BUCKET>.appspot.com",
+  messagingSenderId: "<SENDER_ID>"
 };
